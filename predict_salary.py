727caa6419770a9e545680659c8e4c71b8a00ba3
TNA110 <73946838+TNA110@users.noreply.github.com>
2021-06-19 21:25:20 +0300

    Добавил пробелы между знаками

--- predict_salary.py
+++ predict_salary.py
@@ -1,6 +1,6 @@
 def predict_rub_salary(salary_from, salary_to):
     if salary_from and salary_to:
-        return(int((salary_from+salary_to)/2))
+        return(int((salary_from + salary_to) / 2))
     elif salary_from and not salary_to:
         return(int(1.2 * salary_from))
     elif salary_to and not salary_from:
