8612e57833b3776d3eac6e16e989a058e2db1eec
TNA110 <73946838+TNA110@users.noreply.github.com>
2021-06-19 21:20:25 +0300

    Убрал лишнюю переменную, скорректировал частоту вызова int

--- predict_salary.py
+++ predict_salary.py
@@ -1,10 +1,7 @@
 def predict_rub_salary(salary_from, salary_to):
-    predicted_salary = None
     if salary_from and salary_to:
-        predicted_salary = (int(salary_from)+int(salary_to))/2
+        return(int((salary_from+salary_to)/2))
     elif salary_from and not salary_to:
-        predicted_salary = 1.2 * int(salary_from)
+        return(int(1.2 * salary_from))
     elif salary_to and not salary_from:
-        predicted_salary = 0.8 * int(salary_to)
-    if predicted_salary:
-        return int(predicted_salary)
+        return(int(0.8 * salary_to))
