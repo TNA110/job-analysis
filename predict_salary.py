2b7512f9c540c64e8b2b3dfd3408f5b5eadf613e
TNA110 <73946838+TNA110@users.noreply.github.com>
2021-06-21 21:09:05 +0300

    Убрал лишние скобки

--- predict_salary.py
+++ predict_salary.py
@@ -1,7 +1,7 @@
 def predict_rub_salary(salary_from, salary_to):
     if salary_from and salary_to:
-        return(int((salary_from + salary_to) / 2))
+        return int((salary_from + salary_to) / 2)
     elif salary_from and not salary_to:
-        return(int(1.2 * salary_from))
+        return int(1.2 * salary_from)
     elif salary_to and not salary_from:
-        return(int(0.8 * salary_to))
+        return int(0.8 * salary_to)
